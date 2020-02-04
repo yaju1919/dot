24b410836b035c9d73aec4cf2451a190d82d4616
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-02-05 02:06:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,8 @@
         padding: "1em"
     });
     $("<h1>").text("GUI無しでRPGENを編集するツール").appendTo(h);
-    $("<div>").text("RPGENの作品ページのURLバーにjを入力した後、貼り付けでEnter押してください。").appendTo(h);
+    $("<div>").text("出力は、RPGENの作品ページのURLバーにjを入力した後、貼り付けでEnter押してください。").appendTo(h);
+    $("<div>").text('編集入力欄のid属性は"dq"です。$("#dq").val();などで好きに処理してください。').appendTo(h);
     function addBtn(title, func, parentNode){
         return $("<button>",{text: title}).appendTo(parentNode||h).click(func);
     }
@@ -31,7 +32,7 @@
         title: "編集",
         textarea: true
     });
-    addBtn("出力",function(){
+    addBtn("この内容で出力",function(){
         var str = 'avascript:(function(){var data="' + LZString.compressToEncodedURIComponent(input_dq()) +
             '";(' + toStr(write) + ')();})();';
         yaju1919.addInputText(h_result.empty(),{
@@ -39,8 +40,8 @@
             value: str,
             textarea: true,
             readonly: true
-        },h_ui);
-    });
+        });
+    },h_ui);
     function toStr(func){ // 関数を文字列化
         return String(func).replace(/\/\/.*\n/g,'');
     }
@@ -67,6 +68,6 @@
             isError = true;
             apprise("ごめんなさい・・・<br>マップの書き込みに失敗してしまいました・・(´・ω・｀)ｺﾞﾒﾝ");
             g_oldWriteText = '';
-        })
+        });
     }
 })();
