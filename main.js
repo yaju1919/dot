d7e1ec0061652713b2bf01fd9c9910f201677bdb
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-02-04 23:55:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,22 +6,18 @@
     });
     $("<h1>").text("GUI無しでRPGENを編集するツール").appendTo(h);
     $("<div>").text("avascriptから始まる文字列をrpgenのページのURLバーに貼り付けでEnter押してください。").appendTo(h);
-    function addBtn(title, func){
-        return $("<button>",{text: title}).appendTo(h).click(func);
+    function addBtn(title, func, parentNode){
+        return $("<button>",{text: title}).appendTo(parentNode||h).click(func);
     }
     var h_ui = $("<div>").appendTo(h);
     var h_result = $("<div>").appendTo(h);
     yaju1919.addInputText(h_ui,{
         title: "rpgenのデータをコピーする関数",
         value: (function(){
-            return "avascript:(function(){var c=" +
-                String(yaju1919.copy).replace(/\/\/.*\n/g,'') +
-                ";c(LZString.compressToEncodedURIComponent(getCurrentMapText(dq.mapNum)));alert('コピー完了')})();";
+            return "avascript:(function(){var c=" + toStr(yaju1919.copy) + ';' + toStr(getMapText) +
+                ";c(getMapText());alert('コピー完了')})();";
         })(),
         readonly: true
-    });
-    var input_map = yaju1919.addInputNumber(h_ui,{
-        title: "MAP番号"
     });
     yaju1919.addInputText(h_ui,{
         title: "読み込む",
@@ -35,21 +31,41 @@
         textarea: true
     });
     addBtn("出力",function(){
-        var url = "http://rpgen.us/dq/cons/writeMapText.php",
-            mapText = LZString.compressToEncodedURIComponent(input_dq());
-        var param = {
-            "token": "2afa8b13b99da6535f5ad5040c0f3796b605703b",
-            "index": input_map(),
-            "mapText": mapText
-        };
-        var str = 'avascript:$.post("' + url + '",{' + Object.keys(param).map(function(v){
-            return '"' + v + '":"' + param[v] + '"\n';
-        }) + '});alert("マップの編集が完了しました。");'
+        var str = "avascript:(function(){var data=" + LZString.compressToEncodedURIComponent(input_dq()) + 
+            + '(' + toStr(write) + ')();})();';
         yaju1919.addInputText(h_result.empty(),{
             title: "output",
             value: str,
             textarea: true,
             readonly: true
-        });
+        },h_ui);
     });
+    function toStr(func){ // 関数を文字列化
+        return String(func).replace(/\/\/.*\n/g,'');
+    }
+    function getMapText(){
+        return LZString.compressToEncodedURIComponent(getCurrentMapText(dq.mapNum));
+    }
+    function write(){
+        $.ajax({
+            type: 'POST',
+            url: dqSock.getRPGBase() + 'cons/writeMapText.php',
+            async: false,
+            data: {
+                token: g_token,
+                index: parseInt(dq.mapNum),
+                mapText: data,
+            },
+        }).done(function(r){
+            if ( r != 0 ){
+                isError = true;
+                apprise( "ごめんなさい・・・<br>マップの書き込みに失敗してしまいましたヽ(´Д｀ヽ)(/´Д｀)/ｱﾜﾜ" );
+                g_oldWriteText = '';
+            }
+        }).fail(function(){
+            isError = true;
+            apprise("ごめんなさい・・・<br>マップの書き込みに失敗してしまいました・・(´・ω・｀)ｺﾞﾒﾝ");
+            g_oldWriteText = '';
+        })
+    }
 })();
