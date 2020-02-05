f22bd23d366cec0f974b6df1b60d54da2b1df926
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-02-05 18:23:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -72,36 +72,35 @@
         });
     }
     function makeRandomMap(){ // ランダムMAP化
-    var dq = $("#dq").val();
+        var dq = $("#dq").val();
 
-    function makeRandMap(){
-        return '\n' + yaju1919.makeArray(50).map(function(){
-            return yaju1919.makeArray(50).map(function(){
-                return yaju1919.randInt(1,26104);
-            }).join(' ');
-        }).join('\n');
+        function makeRandMap(){
+            return '\n' + yaju1919.makeArray(50).map(function(){
+                return yaju1919.makeArray(50).map(function(){
+                    return yaju1919.randInt(1,26104);
+                }).join(' ');
+            }).join('\n');
+        }
+        dq = dq.replace(/(?<=#FLOOR)(.|\n)*?(?=#END)/g, makeRandMap);
+
+        function makeRandHuman(){
+            var str = "\n#HUMAN\n";
+            var a = 'A' + yaju1919.randInt(1,1105);
+            return [
+                a,
+                yaju1919.randInt(0,49), // x
+                yaju1919.randInt(0,49), // y
+                2, // direction
+                5, // movement
+                100, // speed
+                '#END\n'
+            ].join(',');
+        }
+        dq = dq.replace(/(?<=#(HUMAN|MAP|EPOINT))(.|\n)*?(?=#END)/g, ''); // 人物イベント全消去
+        yaju1919.makeArray(49).forEach(function(v){
+            dq += makeRandHuman();
+        });
+
+        $("#dq").val(dq);
     }
-    dq = dq.replace(/(?<=#(FLOOR|MAP))(.|\n)*?(?=#END)/g, makeRandMap);
-
-    function makeRandHuman(){
-        var str = "\n#HUMAN\n";
-        var a = 'A' + yaju1919.randInt(1,1105);
-        return [
-            a,
-            yaju1919.randInt(0,299), // x
-            yaju1919.randInt(0,299), // y
-            2, // direction
-            5, // movement
-            100, // speed
-            '#END\n'
-        ].join(',');
-    }
-    dq = dq.replace(/(?<=#HUMAN)(.|\n)*?(?=#END)/g, ''); // 人全消去
-    dq = dq.replace(/(?<=#EPOINT)(.|\n)*?(?=#END)/g, ''); // イベント全消去
-    yaju1919.makeArray(49).forEach(function(v){
-        dq += makeRandHuman();
-    });
-
-    $("#dq").val(dq);
-}
 })();
