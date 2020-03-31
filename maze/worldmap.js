9cee3267483d75266dd001d2240bc22172d68821
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-03-31 16:21:40 +0900

    Update worldmap.js

--- maze/worldmap.js
+++ maze/worldmap.js
@@ -25,25 +25,47 @@
                 [x,y+1],
                 [x,y-1],
             ].filter(v => v[0] < SIZE && v[1] < SIZE && v[0] > 0 && v[1] > 0 && moved.indexOf(v[0] + '_' + v[1]) === -1 && judge_func(v[0], v[1]) );
-            if(!ar) return loop(startX,startY); // 候補がなくなった
+            if(!ar.length) return loop(startX,startY); // 候補がなくなった
             var xy = yaju1919.randArray(ar);
             return loop(xy[0], xy[1]);
         }
     }
     var yukaArray = [];
-    yaju1919.makeArray(5).forEach(v=>{
+    // 最初の地面
+    yaju1919.makeArray(8).forEach(v=>{
         fill(
             yaju1919.randInt(0,SIZE),
             yaju1919.randInt(0,SIZE),
-            30,
+            100,
             (x,y) => {
                 yuka[y][x] = RIKU;
+                if(y > 0) yuka[y-1][x] = RIKU;
+                if(x > 0) yuka[y][x-1] = RIKU;
+                if(y < SIZE - 1) yuka[y+1][x] = RIKU;
+                if(x < SIZE - 1) yuka[y][x+1] = RIKU;
+                yukaArray.push(x + '_' + y);
             },
             (x,y) => {
-                yukaArray.push(x + '_' + y);
                 return true;
             }
         )
+    });
+    // 森
+    [MORI,YAMA,YAMA2].forEach(vv=>{
+        yaju1919.makeArray(8).forEach(v=>{
+            var xy = yaju1919.randArray(yukaArray).split('_').map(v=>Number(v));
+            fill(
+                xy[0],
+                xy[1],
+                100,
+                (x,y) => {
+                    mono[y][x] = vv;
+                },
+                (x,y) => {
+                    return yukaArray.indexOf(x + '_' + y) !== -1;
+                }
+            )
+        });
     });
     (function(){
         var a = yuka.map(line=>line.join(' ')).join('\n');
