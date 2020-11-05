d1c808488f72f5f1b9c1623a47bb703ff95a6bca
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-11-05 14:33:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -80,9 +80,19 @@
             dq(asg);
         }, "#template");
     }
-    addTemplate("ミナギル地方", "MQCQogSg8gUAzAGgOzDAOQCIxsAQgcQFkYALAFzIAcAuAeloHcmA6ATwHsBXMzgIwFNmAY3YBbRgEMyQkgH4AbgF4AKgDYAyqLCiAVssIB9ANapM2PPgCShfKQo16AS2aPRAc04AnYWNoBHRzQARk4AOwATZh1KN1MsHAAxABkoKAh4AwAWAAI4LNz8vJyiguLC8rLK0uqS2oqa+rqqpobmxva2ztbult6O1oyqgE4DAAZskfHJibGZqdnpxYXl+dW5nv6+rq2N7c39vcPdweqltbP1i6uVy5ud+4Pdp4ejl+eOk5Lr85vv9aDVAYgtkAUCQYDgRc3tDHjDXrCEfDMp98tNQcD0eCwZicRCsRi8bjsYS8VDEe8kRSqXDqR8WmiScSmQTmfi2USWZzbuMabzyXzKQKBiUAEwigwi-6Mrkc9nSuWszFkwX81Uq9W0zonC6y3XyvWK+XKzUmoVmtUUmBBPIADm5Cpl+qdhpdkLuFvNGs9prpqJuBsdrodwaV7q9Hoj4ajwr9awDIedgaT9u9qcjPqj2v9iYTQfjobWaejRYzmpROT+ld+1Z+PPTJYb9cR5dLNONjeLTc7SJbHdbXf73cHlr7o4HY6Hnt745nk9nw6L07ny4X84nZfXm7X25XWyXq93O4Px7h+63h4vJ53Z6P56vl8Xd6ft5f3ZvD9f96-j8-z4--8Hd9v1-ECAJeIC-2AsDQJjaC4KghCp0g5CYJQyoINQzD4InDDsKwxDX1wgi8JIqCiLQ4jKJwiiaPw89yLoxjSIFBjmKYqjI1Yji2J4jMuNo3iBOOISRPYn9BLEyS31EiTZMAmTuMU+iFJUqSNzU1S5NPTSlI0zidIMvStUMrSjOEsyTN0ntLJsoz+Ishy4LicxCAAQQABWwLzvJ87I-P8vygiQAwAFYYACgKgtC8KIsC4KwtiuLosSkF4ucnBCHcqBLDQZQrQQVQECCEK4EyIrRgQUZ0uATLstymAKptIqSqakVkHSoA");
-    addTemplate("空室", "MQCQogSg8gUArABgDSOGAcgERjYAhAcQFlcNtdCBJIgmACwBcGAHALgHp2BLAOi4FsA5gFcATjwDGAe37sAjl3QBGYQDsAJjwBWzQWiw5gAMQAyUKBBxXrN23ZsACJ85eu37j55cA2AJwAmJQc-QJgvcIjI5xC4YIC4MKikpLg4XwQHVIAOX0y4HLyC7NziwtysuH8MuG9-LMzagHZE5NavVPSyrtKe-JK+hwqqhrqR5raJjw7qgd6i2YWCoera+pr-ccmt5yV-fwddgGYDvZPjuf6lypXR9c3trd2AFhP9o5OXi67lkbWmloeE12sV2b38x2B3UW5WuvzGAMB22m3UaCCCcFR6MxCMRrRizmRxRxuLahIGBL8vmJJKiHXRaXpvkZQQpvipNMmdLyTO5zN5-IabOpHPCXLFDN5wpFnnFPNlLKcfkO3il0vc8v5XO8tQy2uGeoyFSU7LVKQlGq1OucRpNptp5odTNVdp8tSCevdWSeCGdLsVbuCbt9fqecAAwg5Q2H9ORgEQAIIABXsKdTjj97hCCAjISUOYCebigXzxeCGKeOYxhxLhaVeeDIpCFaL4ZbJdbMUrjX8Xdr8Xz3oj+VRQ6yI8GlXrGczASyEZnhznBLHPYbHJCS-cjV8cBeTmHq+nbg387c293y+7YbXNJPHnPe8yK+vR9cd63O8fB5fr+is9Pb7-k+47DtmT5+D+v5xE8falrmNYIUW1aKgEYFKhWN4kkqHb9m2RZoQW+aHM20GwWRiF1pBUEof4zadnEyrzthQ7eBiLFXg0+QlnOmHrkBLg1E8vgsU8S41JU3FUdR76uIJdH-rxt78X+cGEUWfaHFO1EqZuKk4f4raCT2RY8dpOkAcuSiNCZUlQTJ0TeIcwmBoukmKVhvgwSWyE0QRqn+RpGFmShhw4YOwRerqkUTv43jtvFeFKnF7kea5f6uTGhhgImUCUOgAAqDgMAAHqwiBIAwACeZVKEguCJiAGQMPwrC1bgRAAGoAPphtg6isP4SAAG6tRVrDIKQ2Bxt1iZ9QNw2jWwi4TWgU1gEYXUEJYXAIKwDAALwIAA1BIh0ALQIBdZ1KCtYBrRtW0wDte2HSd52XZdVR1atpAPdtu0Hcdp0fVdhy3fdm3-S9QPvVd3rfXdv2Q09AOvcDV2XeVk1I49z2A29IOXd44M41D+PoyDqIIxDuOozDhNnVkJNoH9KPQwTGNnek1Ptd1YbxjAJUDWyFXVf4IsMONPMs11ECPT901dYmAtC+VVVlcgkvM51XVEMmqitTUWQVaV3hPKLwuDQAzqNgijVb-VtQrDVkD6mWkDleWFULQkW3ATsNU1LVO2GIBKyAMBsMgpXldV-vSy7WBu2QQA");
-    addTemplate("リバース空室", "MQCQogSg8gUArABgDSOGAcgERjYAhAcQFlcNtdCBJIgmACwBcGAHALgHp2BLAOi4FsA5gFcATjwDGAe37sAjl3QBGYQDsAJjwBWzQWiw5gAMQAyUKBBxXrN23fsO7AAheu37j56-f3ANl8ATEpO-kEwPhGRUW6hwbHh0YmJAb5KcAAsIYEIWQE5oTlwcACcwUWlTuVlJcEAHHBKxQlJLT4paZn+AMzFAMJZXbX9BZU1oxVV43UNTa1zXu0ZbpMrY5P1jc3z25W+xbW7cEOHAOz9cL4nAedrtxNjG7M7Oxf7h9e76X2fx6v3-9NNs8XnsDn9qgCplD1jMtsCXiVCkcnvDtnsAnAXOUkbUUajttjRri4fjohcAmDAicsYiicU6U4lAFMkyAoyAl12Zl6nkSaTIuTKQFqYtMUVcQzxfSmZjWezOTKnDyEHz+T5BbthU5RZLkQy5UyFQE2YalXBeWqkhrySLUkVdRKpQ76U6ncrVZbPNaqdq7WK9a6A0HHcH6e7PWTAkLqeMcS7Q86GeGIwKTgglOc0xnKlnhsUgnmCyF8xmPSmPOi4IWq8WMb19ORgEQAIIABUcHc7Xes5ei6OO6Rrg8LQzL5fRCEL2fR05Lea66X6Lj26VnXVLve86MXnw+ler898S+LXRr6LOY5T-fOtSUMb8+dHm6815fj96l4jE6nP9r2Zi76fp6r5pL4XSTp415AZar6QYBz5wRS5y+OkO4VvBCEPquv4zjh+ZdEu35-vOi7QWqeyngetZnvmEEUWhK6zkWuHEaxFEbphbi1LU643mmfEQe0qSHoRxSUdRVH7hJtYXpxyy1CcBGuBRxyIU+clYgpSkuIsvhvkhZH8kcinHrp+nqRpxnab6oHmR+GmaSZrhmWp9kOcZ2bGRBXkjseD4GQ5AHYTpqS+IJdrfCxUVzmxYmkYFyliTWNlHP0ixnNJUlZfmNFdEeDaGGArZQJQ6AACpOAwAAerANEgDAAJ6sF8SC4K2IA5Aw-CsEorXAL0IAAPrtTAbDIDViBIE1LWkNgwDtWQKoFaQxWlRV1W1cgjXNcUfXtZ13W9bgRAAGqDb02DqKwARIAAbqwW0PX1YBzadw2XU9929WwgzILNpBGINBCWFwCCsAwAC8CAANQSFDAC0CCI-DSh-Wgc1gIDwMwKD4NQ7DCNI0jeTPRjWMg2DkMw3DRPI+BpMA0DFN49ThPI+kaMvYz2O41TBO00jk3-Wg5M45T+M08jSNhQzItM2LLP81L8NprLmPy7zEts0jtSc2TGvi6zAvw8UevHWdvTNjAG0BMUu3bbb9tPcL6sQNj6Pm8NVsbZN22+87HtNmdRDtqoPUXLU9U1Sh9VNbbXRIAAzj1SCCCnidXUdgcLVgS1kEAA");
+    yaju1919.addSelect("#template",{
+        title: "テンプレ",
+        list: {
+            "ここから洗濯してね": "",
+            "ミナギル地方": "minagiru",
+            "空室": "kushitsu",
+            "リバース空室": "kushitsu-reverse",
+        },
+        change: function(v){
+            if(!v) return;
+            $.get(`sample/${v}.txt`, dq);
+        }
+    });
     addTemplate("random", makeMapRandom);
     //------------------------------------------------------------------------------------
     //------------------------------------------------------------------------------------
